002fdc40388e382c1bb280e4ba0c0cd7db70fba4
wmufunde <wmmufunde@yahoocom>
2017-07-10 11:09:33 +0100

    added containers and morphtext

---
+++ js/job-title.js
@@ -0,0 +1,11 @@
+$("#job-title").Morphext({
+    // The [in] animation type. Refer to Animate.css for a list of available animations.
+    animation: "tada",
+    // An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+    separator: ",",
+    // The delay between the changing of each phrase in milliseconds.
+    speed: 2000,
+    complete: function () {
+        // Called after the entrance animation is executed.
+    }
+});
